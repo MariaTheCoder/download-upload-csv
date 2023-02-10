a79fd0245d11cadc62f5e7be2517c8f218b8284b
Maria T. Andersen <mteilmann93@gmail.com>
2023-02-11 00:34:07 +0100

    Create mock data to download

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 const downloadBtn = document.getElementById("download-btn");
+const data = {
+  id: 1,
+  name: "Maria",
+  isSmart: true,
+};
 
 downloadBtn.addEventListener("click", () => {
   console.log("Hello World");
