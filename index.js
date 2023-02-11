8a02ea8adfd66a40f061ad56005abe17d2314594
Maria T. Andersen <mteilmann93@gmail.com>
2023-02-11 01:18:22 +0100

    Initialize a variable called csv which is to eventually contain all of the data we want to download into a csv file. The variable should be of type string and contain the desired headers for our data deparated by commas.

--- index.js
+++ index.js
@@ -8,5 +8,12 @@ const data = [
 downloadBtn.addEventListener("click", downloadCSV);
 
 function downloadCSV() {
+  /**
+   * In order for this particular solution to work, we need to work with data types of strings.
+   * The following variable is to contain all data we want in our downloaded csv-file.
+   * Firstly, we need to add the headers of the data we want to download
+   */
+  let csv = "Id,Name,isSmart\n";
+
   console.log("Hello World");
 }
