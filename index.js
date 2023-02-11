77874648af0a6f1c35a56360cc544760b2e0acb7
Maria T. Andersen <mteilmann93@gmail.com>
2023-02-11 11:20:08 +0100

    Create a new link element which refers to data of the type text. Make sure to convert the content of the csv variable so each character is represented by the UTF-8 encoding of the characters. Then use the target attribute to ensure that the file is not opened in the current tab.

--- index.js
+++ index.js
@@ -25,5 +25,25 @@ function downloadCSV() {
     csv += "\n";
   });
 
+  /**
+   * The goal is to eventually be able to download a csv-file upon button click and the file should contain the content of the csv variable.
+   * The content of the csv variable can then be displayed in Excel or similar software in a table.
+   *
+   * We do this by creating a hidden element which links to the csv file and call the click method on this element within this function to simulate a mouse click.
+   * By doing this we make it so the desired file is downloaded when this function is called.
+   *
+   * In this example, we accomplish this by creating a new anchor (or "link") element within the document.
+   * We then set the destination of the link to be a data file of the type text and subtype csv-file. The link is now a hyperlink to a csv data file.
+   * By adding the "charset" parameter, we specify the character set used for the characters in the data.
+   * The function encodeURI() is then used on the csv variable to convert each character within the variable to be represented by the UTF-8 encoding of the characters.
+   * The output of the encodeURI(csv) is added to the hyperlink reference.
+   *
+   * Then we add the attribute target to the link and give it the value of _blank to ensure that the file is not opened in the current tab.
+   * This particular step has been historically important as not all browsers opened links in the same way. This step simply exists to ensure that all browsers, and all versions, open the link in the same way.
+   */
+  let hiddenElement = document.createElement("a");
+  hiddenElement.href = "data:text/csv;charset=utf-8," + encodeURI(csv);
+  hiddenElement.target = "_blank";
+
   console.log(csv);
 }
