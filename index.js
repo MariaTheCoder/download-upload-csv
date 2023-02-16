e2dfc1611ee080f893d801cdcc468dc5ef1ab551
Maria T. Andersen <mteilmann93@gmail.com>
2023-02-16 19:04:46 +0100

    Get access to the content of uploaded file with the FileReader object and use its instance method readAsText to get a string containing the content of the uploaded file.

--- index.js
+++ index.js
@@ -37,6 +37,20 @@ function readCSV() {
   }
 
   console.log("Oy, some data was loaded!");
+
+  /**
+   * If a file was correctly selected, use the FileReader object to see the content of the file.
+   * Get access to selected file by using the files property of the HTMLInputElement uploadBtn.
+   * This returns a FileList object. Save this into a constant variable called 'file'.
+   *
+   * To read the file, we can use a FileReader object.
+   * Construct a new FileReader object and save it into a constant variable called 'fileReader'.
+   * The FileReader object has an instance method called readAsText() which reads the content of a file.
+   * The content of the file is then stored inside of the result attribute in the form of a string.
+   */
+  const file = uploadBtn.files[0];
+  const fileReader = new FileReader();
+  fileReader.readAsText(file);
 }
 
 function downloadCSV() {
