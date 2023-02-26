31d016a96fb4e7deb42e7465305066d38c462643
Maria T. Andersen <mteilmann93@gmail.com>
2023-02-26 12:54:06 +0100

    If a csv file is successfully uploaded, replace the content of the existing data variable with the content of the uploaded csv file

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const downloadBtn = document.getElementById("download-btn");
 const gridContainer = document.getElementById("grid-container");
 const uploadBtn = document.getElementById("upload");
-const data = [
+let data = [
   [1, "Maria", true],
   [2, "Kerim", true],
   [3, "Ms. DumbDumb", false],
@@ -62,43 +62,59 @@ function readCSV() {
    * We listen for the event "load" and look into the result attribute of the fileReader object once the fileReader object has finished loading.
    */
   fileReader.addEventListener("load", () => {
+    let data = [];
+    let newHeaders = "";
+
     /**
      * Now we iterate over the result with two goals.
      * For starts, we want to make sure to cut loose the invisible \r at the end of each string line.
      * The invisible character is attached to the value of isSmart in each line, a.k.a. the value in the last column.
      * We can cut this character out with the slice method.
-     * Secondly, we want to create a new grid item for each cell
+     * Secondly, we want to replace the value of the variable data and change its content content with the content of the file that was just uploaded
      */
 
-    fileReader.result.split("\n").forEach((row) => {
-      for (let i = 0; i < row.split(";").length; i++) {
-        const cell = row.split(";")[i];
+    for (let i = 1; i < fileReader.result.split("\n").length; i++) {
+      const row = fileReader.result.split("\n")[i];
+      const rowArray = [];
+
+      for (let j = 0; j < row.split(";").length; j++) {
+        const cell = row.split(";")[j];
 
         if (cell.includes("\r")) {
           const newCell = cell.replace("\r", "");
 
-          let gridItem = document.createElement("div");
-          gridItem.setAttribute("class", "grid_element");
-          gridItem.innerHTML = newCell;
-          gridContainer.appendChild(gridItem);
+          rowArray.push(newCell);
         } else {
-          let gridItem = document.createElement("div");
-          gridItem.setAttribute("class", "grid_element");
-          gridItem.innerHTML = cell;
-          gridContainer.appendChild(gridItem);
+          rowArray.push(cell);
         }
       }
-    });
 
-    const newHeaders = fileReader.result.split("\n")[0];
-    let records = "";
-
-    for (let i = 1; i < fileReader.result.split("\n").length - 1; i++) {
-      const record = fileReader.result.split("\n")[i];
-
-      records += record;
-      records += "\n";
+      data.push(rowArray);
     }
+
+    /**
+     * Save the new headers in the same format as the csv variable from earlier
+     */
+    for (let i = 0; i < 1; i++) {
+      const row = fileReader.result.split("\n")[i];
+
+      for (let j = 0; j < row.split(";").length; j++) {
+        const cell = row.split(";")[j];
+
+        console.log("cell: ", cell);
+
+        if (cell.includes("\r")) {
+          const newCell = cell.replace("\r", "");
+
+          newHeaders += `${newCell}\n`;
+        } else {
+          newHeaders += `${cell},`;
+        }
+      }
+    }
+
+    console.log("data: ", data);
+    console.log("newHeaders: ", newHeaders);
 
     /**
      * Check if the headers of the uploaded file are identical to the headers used in the grid
